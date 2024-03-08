23ae21a8cd84dd6ae3c9011d3477b8b2b073a6be
sp3767 <sebasperezpan@gmail.com>
2024-03-07 22:02:33 -0500

    s

--- clase_1/for.py
+++ clase_1/for.py
@@ -1,9 +1,9 @@
-sum = int(input("numero."))
-d = 2
-while sum % d != 0:
-    d += 1 
+for sum in range(2,101):
+    d = 2
+    while sum % d != 0:
+        d += 1 
 
-if d == sum:
-    print("Es primo")
-else:
-    print("No es.")
+    if d == sum:
+        print("Es primo")
+    else:
+        print("No es.")
