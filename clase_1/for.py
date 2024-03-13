37683ab6a9723a594235b0773b24535b03c05c8f
sp3767 <sebasperezpan@gmail.com>
2024-03-13 01:13:53 -0500

    s

--- clase_1/for.py
+++ clase_1/for.py
@@ -1,9 +1,10 @@
-for sum in range(2,101):
+lista_xd = []
+for sum in range(2,100011):
     d = 2
     while sum % d != 0:
         d += 1 
 
     if d == sum:
-        print("Es primo")
-    else:
-        print("No es.")
+        lista_xd.append(sum)
+
+print(f"los numeros primos son: \n{lista_xd}")
